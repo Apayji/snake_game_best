7fca5b73b39b90ae14c560167ebde07ac7db2070
Apayji <chalakfarzad@gmail.com>
2022-02-27 19:31:46 +0600

    None and Some

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,52 @@
+use std::io;
 use std::time::Duration;
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
-// ☼
 fn main() {
-    let mut x=6;
-    let mut y=15;
+    let mut direction: Option<Direction> = None;
+    let mut x = 6;
+    let mut y = 15;
     let mut map = vec![vec![0; 50]; 12];
     for (i, row) in map.iter().enumerate() {
         for (j, _) in row.iter().enumerate() {
             if i == 0 || i == 11 || j == 0 || j == 49 {
                 print!("█");
-            }
-            else if i==x && j==y {
-                print!("☼"); 
-            }
-             else {
+            } else if i == x && j == y {
+                print!("☼");
+            } else {
                 print!(" ");
             }
         }
         println!("");
     }
-    
-    loop{
-        x-=1;
-        std::thread::sleep(Duration::from_secs(1));
-        for (i, row) in map.iter().enumerate() {
-            for (j, _) in row.iter().enumerate() {
-                if i == 0 || i == 11 || j == 0 || j == 49 {
-                    print!("█");
+    match direction {
+        None => {
+            let mut a = String::new();
+            io::stdin().read_line(&mut a).expect("Couldn't read line");
+        }
+        Some(_) => loop {
+            x -= 1;
+            std::thread::sleep(Duration::from_secs(1));
+            for (i, row) in map.iter().enumerate() {
+                for (j, _) in row.iter().enumerate() {
+                    if i == 0 || i == 11 || j == 0 || j == 49 {
+                        print!("█");
+                    } else if i == x && j == y {
+                        print!("☼");
+                    } else {
+                        print!(" ");
+                    }
                 }
-                else if i==x && j==y {
-                    print!("☼"); 
-                }
-                 else {
-                    print!(" ");
-                }
+                println!("");
             }
-            println!("");
-        }
-        if x==49 || x==0 {
-            break;
-        }
+            if x == 49 || x == 0 {
+                break;
+            }
+        },
     }
 }
