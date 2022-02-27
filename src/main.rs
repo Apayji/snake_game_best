11c1c4672de42bce4e28c5fc9252887433facd62
Apayji <chalakfarzad@gmail.com>
2022-02-27 19:12:43 +0600

    snake head moving

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
+use std::time::Duration;
+
+// ☼
 fn main() {
-    let map = [[0; 50]; 12];
+    let mut x=6;
+    let mut y=15;
+    let mut map = vec![vec![0; 50]; 12];
     for (i, row) in map.iter().enumerate() {
         for (j, _) in row.iter().enumerate() {
             if i == 0 || i == 11 || j == 0 || j == 49 {
                 print!("█");
-            } else {
+            }
+            else if i==x && j==y {
+                print!("☼"); 
+            }
+             else {
                 print!(" ");
             }
         }
         println!("");
+    }
+    
+    loop{
+        x-=1;
+        std::thread::sleep(Duration::from_secs(1));
+        for (i, row) in map.iter().enumerate() {
+            for (j, _) in row.iter().enumerate() {
+                if i == 0 || i == 11 || j == 0 || j == 49 {
+                    print!("█");
+                }
+                else if i==x && j==y {
+                    print!("☼"); 
+                }
+                 else {
+                    print!(" ");
+                }
+            }
+            println!("");
+        }
+        if x==49 || x==0 {
+            break;
+        }
     }
 }
