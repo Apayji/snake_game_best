d019b0f64cc98fc8ff4a0aa53b4253d8b624f729
Apayji <chalakfarzad@gmail.com>
2022-02-27 18:55:10 +0600

    Map of hell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let map = [[0; 50]; 12];
+    for (i, row) in map.iter().enumerate() {
+        for (j, _) in row.iter().enumerate() {
+            if i == 0 || i == 11 || j == 0 || j == 49 {
+                print!("█");
+            } else {
+                print!(" ");
+            }
+        }
+        println!("");
+    }
 }
